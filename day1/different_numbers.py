ff08e6b933192d32d1e65b96119678c77c8c3ab1
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2025-04-21 12:15:49 +0530

    Update different_numbers.py

--- day1/different_numbers.py
+++ day1/different_numbers.py
@@ -8,6 +8,7 @@
 8.adam's
 9.magic
 '''
+'''
 a = 145
 t = a
 s = 0
@@ -23,5 +24,37 @@ if t == s:
 else:
     print("fail")
 
-    
-    
+'''
+
+'''
+a = 156
+t = a
+s = 0
+while a>0:
+    d = a%10
+    s += d
+    a//=10
+if t%s == 0:
+    print('nivens number')
+else:
+    print("not")
+'''
+
+'''
+num = int(input("enter a number :"))
+visit = set()
+while num!=1 and num not in visit:
+    visit.add(num)
+    s = 0
+    temp = num
+    while temp>0:
+        d = temp%10
+        s+=d**2
+        temp//=10
+    num = s
+if num == 1:
+    print("happy")
+else:
+    print("not happy")
+
+'''
