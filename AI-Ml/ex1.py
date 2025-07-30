353f36dcb2b2c9db2defc5a7c41e06c21ab31461
U-D-AY <chilamkurthiudaysrinadh@gmail.com>
2025-07-30 16:06:51 +0530

    Add files via upload

---
+++ AI-Ml/ex1.py
@@ -0,0 +1,11 @@
+"""
+
+Code to print a 3x3 matrix which were filled with boolen values True using
+numpy
+
+"""
+import numpy as np
+
+# print("Array: \n", np.bool(np.ones((3,3))))
+
+print("Array: \n", np.full((3, 3), True))
